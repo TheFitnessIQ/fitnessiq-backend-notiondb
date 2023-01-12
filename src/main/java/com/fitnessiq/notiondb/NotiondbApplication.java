86e05119a48e9a0688bc34e43d7a5a0444bdd1e5
Rajib Sahoo <rajibborn2win@gmail.com>
2023-01-12 17:42:10 +0530

    Initial Commit

---
+++ src/main/java/com/fitnessiq/notiondb/NotiondbApplication.java
@@ -0,0 +1,13 @@
+package com.fitnessiq.notiondb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotiondbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotiondbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitnessiq/notiondb/NotiondbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessiq.notiondb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotiondbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
